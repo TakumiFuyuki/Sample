4067e4662f03b371a8d7eaafbc0a446beab7a719
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-13 12:29:29 +0900

    ユーザーごと

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from google.cloud import bigquery
 from google.cloud import storage
 from datetime import datetime
@@ -15,6 +15,7 @@ storage_client = storage.Client()
 
 dataset_name = 'my-project-sample-425203.dataset'
 registration_table = 'registration'
+file_table = 'user_files'
 bucket_name = 'txt_submit'
 bucket = storage_client.bucket(bucket_name)
 
@@ -58,6 +59,58 @@ def login():
     else:
         flash('メールアドレスかパスワードが異なります。')
         return redirect(url_for('login_page'))
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and file.filename.endswith('.txt'): #txtファイルに制限
+            blob = bucket.blob(file.filename)
+            blob.upload_from_string(file.read(), content_type=file.content_type)
+            flash(f'File {file.filename} uploaded to {bucket_name}.')
+            return redirect(url_for('index'))
+        else:
+            flash('Only .txt files are allowed.')
+            return redirect(url_for('upload_file'))
+    return render_template('upload.html')
+
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    if 'user' not in session:
+        return redirect(url_for('login_page'))
+    user_email = session['user']
+    if not is_user_file(user_email, filename):
+        flash('You are not authorized to download this file.')
+        return redirect(url_for('list_files'))
+    blob = bucket.blob(filename)
+    file_content = blob.download_as_string()
+    return file_content, 200, {
+        'Content-Disposition': f'attachment; filename={filename}',
+        'Content-Type': blob.content_type
+    }
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+@app.route('/files')
+def list_files():
+    if 'user' not in session:
+        return redirect(url_for('login_page'))
+    user_email = session['user']
+    files = get_user_files(user_email)
+    return render_template('files.html', files=files)
+
+def get_user_files(email):
+    query = f"""
+    SELECT file_name, upload_time FROM `{dataset_name}.{file_table}`
+    WHERE user_email = '{email}'
+    ORDER BY upload_time DESC
+    """
+    query_job = bigquery_client.query(query)
+    results = query_job.result()
+    return [{'file_name': row['file_name'], 'upload_time': row['upload_time']} for row in results]
 
 def insert_registration_to_bigquery(email, button_time, password):
     button_time_iso = button_time.isoformat()
@@ -106,28 +159,30 @@ def authenticate_user(email, password):
             return True
     return False
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and file.filename.endswith('.txt'): #txtファイルに制限
-            blob = bucket.blob(file.filename)
-            blob.upload_from_string(file.read(), content_type=file.content_type)
-            flash(f'File {file.filename} uploaded to {bucket_name}.')
-            return redirect(url_for('index'))
-        else:
-            flash('Only .txt files are allowed.')
-            return redirect(url_for('upload_file'))
-    return render_template('upload.html')
+def insert_file_record(email, file_name):
+    rows_to_insert = [
+        {
+            'user_email': email,
+            'file_name': file_name,
+            'upload_time': datetime.now().isoformat()
+        }
+    ]
+    table_id = f'{dataset_name}.{file_table}'
+    errors = bigquery_client.insert_rows_json(table_id, rows_to_insert)
+    if errors:
+        raise Exception(f'BigQueryへのファイル情報挿入中にエラーが発生しました: {errors}')
 
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename):
-    blob = bucket.blob(filename)
-    file_content = blob.download_as_string()
-    return file_content, 200, {
-        'Content-Disposition': f'attachment; filename={filename}',
-        'Content-Type': blob.content_type
-    }
+def is_user_file(email, file_name):
+    query = f"""
+    SELECT COUNT(1) as count FROM `{dataset_name}.{file_table}`
+    WHERE user_email = '{email}' AND file_name = '{file_name}'
+    """
+    query_job = bigquery_client.query(query)
+    results = query_job.result()
+    for row in results:
+        if row['count'] > 0:
+            return True
+    return False
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
