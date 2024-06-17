1374e1f8a29068ed9c2d1685f67f5cec645a6fc6
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-17 13:24:01 +0900

    redirect_url_for

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return render_template('main.html')
+        return redirect(url_for('main'))
     return render_template('login.html')
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
