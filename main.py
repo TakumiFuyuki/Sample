508d65932c7dd0b8cbc82fba61520e291b23e3a2
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-17 12:16:14 +0900

    logintest

--- main.py
+++ main.py
@@ -22,5 +22,9 @@ bucket = storage_client.bucket(bucket_name)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def index():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
