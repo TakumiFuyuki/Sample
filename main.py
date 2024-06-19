26a2826ce4ec84853366e67fee97ee7d86b7696b
Takumi Fuyuki <fuyuki0121ta@gmail.com>
2024-06-19 14:36:51 +0900

    kon

--- main.py
+++ main.py
@@ -88,8 +88,11 @@ def main():
 
     try:
         email = session['email']
+        print(email)
         files = utils.get_user_files(email)
-        return render_template('main.html', files=files)
+        print(files)
+        return 'kon'
+        # return render_template('main.html', files=files)
     except Exception as e:
         app.logger.error(f"メインページの読み込み中にエラーが発生しました: {e}")
         return "エラーが発生しました", 500
